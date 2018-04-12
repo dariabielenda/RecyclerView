2d19002ad1b46ca56cea966565a156fdd029ee55
Michał Kuchta <w57016@student.wsiz.rzeszow.pl>
2018-04-12 18:31:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stud/myapplication/Planeta.java
@@ -0,0 +1,16 @@
+package com.example.stud.myapplication;
+
+
+public class Planeta {
+
+     String nazwa;
+     String rodzaj;
+     int ksiezyce;
+
+     public Planeta (String nazwa, String rodzaj, int ksiezyce) {
+         this.nazwa = nazwa;
+         this.rodzaj = rodzaj;
+         this.ksiezyce = ksiezyce;
+     }
+
+}
